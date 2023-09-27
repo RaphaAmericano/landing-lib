310709aabd677e6929db314aac9dd4a972cd8b29
Raphael Americano <raphaelamericano@Raphaels-MacBook-Pro.local>
2023-09-27 00:03:07 -0300

    add tailwind

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -5,7 +5,9 @@ export interface ButtonProps {
 }
 
 const Button = ({ label }: ButtonProps ) => {
-    return <button>{label}</button>
+    return <button 
+        className="inline-flex items-center rounded-md bg-red px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+    >{label}</button>
 }
 
 export default Button;
