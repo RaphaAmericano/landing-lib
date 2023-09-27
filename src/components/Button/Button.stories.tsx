44f56da1d237a4704aefdae4991dd124f8456cb7
Raphael Americano <raphaelamericano@Raphaels-MacBook-Pro.local>
2023-09-26 23:25:50 -0300

    add babel

---
+++ src/components/Button/Button.stories.tsx
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from "@storybook/react"
+import Button from "./Button"
+
+const meta: Meta<typeof Button> = {
+    title:"Button",
+    component: Button,
+}
+type Story = StoryObj<typeof Button>
+
+export const Primary: Story = {
+    args: {
+        label: "Primary"
+    } 
+}
+
+export const Secondary: Story = {
+    args: {
+        ...Primary.args, 
+        label: "Secondary"
+    }
+}
+
+export default meta;
