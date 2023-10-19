085afb53ef33c5f93616dca02a656f0a620d26e5
Raphael Americano <raphaelamericano@Raphaels-MacBook-Pro.local>
2023-10-19 01:03:02 -0300

    remove tests before debug

---
+++ src/components/Paragraph/Paragraph.tests.tsx
@@ -0,0 +1,20 @@
+import React from "react"
+import { fireEvent, render, screen } from "@testing-library/react"
+import { composeStories } from "@storybook/react"
+import * as stories from "./Paragraph.stories"
+
+const { ParagraphBasic } = composeStories(stories)
+
+describe("Paragraph test", () => {
+
+    test("renders primary heading with default values", () => {
+        // render(<ParagraphBasic />)
+        // const paragraphElement = screen.getByRole("p", {
+        //     name:"Text",
+        // });
+        // fireEvent.click(paragraphElement)
+        // expect(paragraphElement).not.toBeNull()    
+        const num = 1;
+        expect(num).toBe(1)
+    })
+})
