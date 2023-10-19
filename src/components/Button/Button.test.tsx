4e71ddb1d15ab39458aada01ce164ee8a9905880
Raphael Americano <raphaelamericano@Raphaels-MacBook-Pro.local>
2023-10-19 01:39:50 -0300

    remove test

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -2,8 +2,8 @@ import React from "react"
 import { render, screen } from "@testing-library/react"
 import { composeStories } from "@storybook/react"
 import * as stories from "./Button.stories"
+// const { Primary } = composeStories(stories)
 
-const { Primary } = composeStories(stories)
 describe("Button test", () => {
     test("renders primary button with default values", () => {
         const num = 1
