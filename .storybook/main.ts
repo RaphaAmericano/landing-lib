95c3405c34115584199eb40774c5f0e594d04a2f
Raphael Americano <raphaelamericano@Raphaels-MacBook-Pro.local>
2023-10-20 00:57:48 -0300

    config jest addon

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -21,6 +21,7 @@ const config: StorybookConfig = {
         },
       },
     },
+    "@storybook/addon-jest"
   ],
   framework: {
     name: "@storybook/react-webpack5",
--- jest.config.ts
+++ jest.config.ts
@@ -93,7 +93,7 @@ const config: Config = {
   // moduleNameMapper: {},
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
-  // modulePathIgnorePatterns: [],
+  modulePathIgnorePatterns: ['node_modules', 'jest-test-results.json'],
 
   // Activates notifications for test results
   // notify: false,
