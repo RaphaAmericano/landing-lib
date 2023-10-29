a5d0296a56803cde509146eef79a2d06be398042
Raphael Americano <raphaelamericano@Raphaels-MacBook-Pro.local>
2023-10-29 14:28:09 -0300

    add test example

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -11,22 +11,30 @@ import * as stories from './Button.stories';
 const { Active, Primary } = composeStories(stories);
 
 describe("Button Test", () => {
-    test('Validates form', () => {
+  test('Validates form', () => {
       render(<Primary />);
-    
-      // const buttonElement = screen.getByRole('button', {
-      //   name: 'Submit',
-      // });
+      const buttonElement = screen.getByRole('button', {
+        name: 'Primary',
+      });
+      
       
       // fireEvent.click(buttonElement);
-      // const childrenText = screen.getByText("Primary") 
+      const childrenText = screen.getByText("Primary") 
       // const isFormValid = screen.getByLabelText('invalid-form');
       // expect(childrenText).not.toBeInTheDocument();
     });
 
-    test("shoud num be 1", () => {
-      const num = 1;
-      expect(num).toBe(1)
+    test("shoud children be text = 'Primary'", () => {
+      const rendered = render(<Primary />);
+      const { container } = rendered;
+      expect(container.textContent).toEqual("Primary")
+    } )
+
+    test("shoud children be text", () => {
+      render(<Primary />);
+      const buttonElement = screen.getByRole('button');
+      console.log(buttonElement)
+      expect(buttonElement.textContent).toEqual(Primary.args!.children)
     } )
 
 })
