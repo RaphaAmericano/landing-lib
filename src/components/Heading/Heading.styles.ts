c43f52830f6c17c33808c683837702d196909934
Raphael Americano <raphaelamericano@Raphaels-MacBook-Pro.local>
2023-11-11 12:56:41 -0300

    first tile skeleton

--- src/components/Heading/Heading.styles.ts
+++ src/components/Heading/Heading.styles.ts
@@ -4,7 +4,7 @@ import type { HeadingStyleProps, HeadingTag } from "./Heading.props"
 
 function HeadingStyle(tag: HeadingTag){
     return styled[tag].attrs((attrs) => ({
-            className: clsx(attrs.className, `my-4 text-5xl font-bold leading-tight`)
+            className: clsx(attrs.className, `font-bold leading-tight`)
         }))<HeadingStyleProps>``
 }
 
--- src/components/Paragraph/Paragraph.styles.ts
+++ src/components/Paragraph/Paragraph.styles.ts
@@ -1,8 +1,9 @@
 import styled from "styled-components"
+import clsx from "clsx"
 import type { ParagraphStyleProps } from "./Paragraph.props"
 
 const  ParagraphStyle = styled.p.attrs((attrs) => ({
-    className: attrs.className ? `leading-normal text-2xl mb-8 ${attrs.className}` : `leading-normal text-2xl mb-8`
+    className: clsx(`leading-normal text-2xl mb-8`, attrs.className)
 }))<ParagraphStyleProps>``
 
 export default ParagraphStyle;
